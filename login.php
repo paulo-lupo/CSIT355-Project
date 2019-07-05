1998f6a5e8ddad6292734277f80d96d2c9ff2192
Paulo Ferreira <paulof.dev@gmail.com>
2019-07-05 10:16:27 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+
+if (@$_POST["submit"] <> "") {
+
+	// Setup variables
+	$Username = @$_POST["username"];
+	$Password = @$_POST["password"];
+   
+ }
+  $str = "Hello world!";
+    echo $Username;
+
+?>
